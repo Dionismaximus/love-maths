082df989be580f2ba868d804ed6cd792bc726d34
Denis Litvinenko <dionismaximus666@gmail.com>
2024-04-04 14:07:55 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2)
     } else if (gameType === 'subtract') {
         displaySubstractQuestion(num1, num2)
+    } else if (gameType === 'division') {
+        displayDivisionQuestion(num1, num2)
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`
@@ -81,6 +83,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, 'multiply'];
     } else if (operator === '-') {
         return [operand1 - operand2, 'subtract'];
+    } else if (operator === '/') {
+        return [operand1 / operand2, 'division']
     } else {
         alert(`Unemplemented operator ${operator}`);
         throw `Unemplemented operator ${operator}. Aborting!`;
@@ -121,4 +125,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = 'x';
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '/';
 }
